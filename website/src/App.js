b545f8062201d62ec33a3e755a5349e2926c026e
Fabien Tilly <fatilly@z2r3p7.42lyon.fr>
2022-09-15 16:59:45 +0200

    color bg

--- website/src/App.js
+++ website/src/App.js
@@ -3,23 +3,14 @@ import './App.css';
 
 function	App()
 {
-	// const body = document.querySelector("body");
-	// const h1 = document.querySelectorAll("h1");
-
-	// h1.foreach(el => {
-	// 	el.addEventListener("mouseover", () => {
-	// 		let bg = el.getAttribute("data-bg");
-	// 		body.style.background = '${bg}';
-	// 	});
-	// });
 	return (
 		<React.Fragment>
 			<div className="App">
-				<h1 data-bg="adff2f">TRANSCENDANCE</h1>
-			</div>
-			<div className="Content">
-				<h1>PLAY</h1>
-			</div>
+				<ul>
+					
+				</ul>
+        		<h1>TRANSCENDANCE</h1>
+    		</div>
 		</React.Fragment>
 	);
 }
